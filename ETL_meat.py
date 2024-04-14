d4c41b1993b19e7e0c4e370f20ac4eca1cca02f3
yctsaiyc <yctsaiyc@gmail.com>
2024-04-14 18:12:17 +0800

    Format code

--- ETL_meat.py
+++ ETL_meat.py
@@ -7,23 +7,24 @@ import time
 import os
 import schedule
 
+
 class ExchangeFetcher(ETLprocessorLive):
     def __init__(self, config_file) -> None:
-        with open(config_file,'r') as file:
+        with open(config_file, "r") as file:
             self.config = json.load(file)
-    
+
     def process_api(self, api_name):
         return super().process_api(api_name)
-    
+
     def process_data_api(self, api_url, map_columns, path):
         try:
             r = requests.get(api_url)
             content = json.loads(r.text)
             df = pd.DataFrame(content)
             # current_date = datetime.now().date()
-            date_list = ['日期' , 'date' , 'transDate']
-            if '交易日期' in df.columns:
-                df['交易日期'] = df['交易日期'].apply(self.roc_to_ad)
+            date_list = ["日期", "date", "transDate"]
+            if "交易日期" in df.columns:
+                df["交易日期"] = df["交易日期"].apply(self.roc_to_ad)
             for date in date_list:
                 if date in df.columns:
                     df[date] = df[date].apply(self.format_ad)
@@ -34,13 +35,14 @@ class ExchangeFetcher(ETLprocessorLive):
             print(csv_file_path)
             df.to_csv(csv_file_path, index=False)
         except Exception as e:
-            print("ERROR:" ,e)
+            print("ERROR:", e)
+
 
 if __name__ == "__main__":
     etl_processor = ExchangeFetcher("api_config_meat.json")
     # print(etl_processor.process_api("API1"))
-#    def day_job():
-    api_live_list_day=[f'API{i}' for i in range(1,9)]
+    #    def day_job():
+    api_live_list_day = [f"API{i}" for i in range(1, 9)]
     for api in api_live_list_day:
         etl_processor.process_api(api)
 #    schedule.every().day.at("00:00").do(day_job)
--- ETL_tool.py
+++ ETL_tool.py
@@ -2,14 +2,15 @@ import json
 
 
 class ETLprocessor:
-    def __init__(self,config_file) -> None:
-        with open(config_file,'r') as file:
-            self.config =json.load(file)
-    def process_api(self , api_name):
+    def __init__(self, config_file) -> None:
+        with open(config_file, "r") as file:
+            self.config = json.load(file)
+
+    def process_api(self, api_name):
         if api_name in self.config:
             api_info = self.config[api_name]
             api_url = api_info.get("url")
-            store_path = api_info.get('path')
+            store_path = api_info.get("path")
             data_processor_name = api_info.get("data_processor")
             map_columns = api_info.get("columns")
             if api_url and data_processor_name:
@@ -19,12 +20,10 @@ class ETLprocessor:
                 # 動態調用對應的資料處理方法
                 data_processor = getattr(self, data_processor_name, None)
                 if callable(data_processor):
-                    data_processor(api_url,map_columns,store_path)
+                    data_processor(api_url, map_columns, store_path)
                 else:
                     print(f"Invalid data processor for API: {api_name}")
             else:
                 print(f"Invalid configuration for API: {api_name}")
         else:
             print(f"API not found: {api_name}")
-
-
