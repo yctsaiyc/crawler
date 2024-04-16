92bd7188cb2bf2105bfd29fbfdb5f88f0f0b8be8
yctsaiyc <yctsaiyc@gmail.com>
2024-04-16 16:24:40 +0800

    ETL_meat: writing the latest_data_date to config after the first data download

--- ETL_meat.py
+++ ETL_meat.py
@@ -63,68 +63,81 @@ class ExchangeFetcher(ETLprocessorLive):
                 break
         return df
 
-    def process_data_api(self, api_name, api_url, map_columns, dir_path):
+    def update_config_latest_data_date(self, api_name, last_row):
+        for col_name in ["date", "transDate"]:
+            try:
+                latest_data_date = last_row[col_name]
+                self.config[api_name]["latest_data_date"] = latest_data_date
+                with open("api_config_meat.json", "w", encoding="utf-8") as file:
+                    json.dump(self.config, file, ensure_ascii=False, indent=4)
+                print(f"Update {api_name} latest_data_date: {last_row[col_name]}")
+                break
+            except KeyError:
+                pass
+
+    def process_data_api(self, api_name, map_columns, dir_path):
         today = datetime.today().date()
         os.makedirs(dir_path, exist_ok=True)
+        print("Get api info...")
+        url = self.config[api_name]["url_opendata"]
+        print("  url:", url)
+        latest_data_date = self.config[api_name]["latest_data_date"]
+        print("  latest data date:", latest_data_date)
         print("Get csv file path...")
         csv_file_path = dir_path + dir_path.split("/")[-2] + ".csv"
         print("  csv file path:", csv_file_path)
-        print("  latest data date:", self.dic_latest_data_date[api_name])
-        # if self.dic_latest_data_date[api_name] is None:
         if not os.path.exists(csv_file_path):
             print("First-time data download...")
             print("  Get json data and convert to dataframe...")
-            print("    api_url:", api_url)
-            df = self.fetch_json_to_df(api_url)
+            df = self.fetch_json_to_df(url)
             print("  Reverse the order of data with old data at the top...")
             df = df[::-1].reset_index(drop=True)
             print(df.tail(10))
-            print(
-                "  Delete the data of the two most recent dates (To test update function)"
-            )
-            df = self.del_data_of_recent_dates(api_name, df, 2)
-            print("  Write to csv")
+            # print(
+            #     "  Delete the data of the two most recent dates (To test update function)"
+            # )
+            # df = self.del_data_of_recent_dates(api_name, df, 2)
+            print("  Write to csv...")
             with open(csv_file_path, "w") as f:
                 df.to_csv(f, index=False, header=False)
-            # Update latest_data_date
-            # self.dic_latest_data_date[api_name] = today
-        else:
-            print("Update csv file...")
-            df = None
-            latest_data_date = self.dic_latest_data_date[api_name]
-            while latest_data_date < today:
-                latest_data_date += timedelta(days=1)
-                # Append parameters to the original URL
-                if api_name == "API1":
-                    date_str = self.date_to_roc_year_str(latest_data_date)
-                else:
-                    date_str = latest_data_date.strftime("%Y/%m/%d")
-                print("  Current date:", date_str)
-                updated_api_url = api_url + date_str
-                if df is None:
-                    df = self.fetch_json_to_df(updated_api_url)
-                    for date in ["日期", "date", "transDate"]:
-                        if df is not None and date in df.columns:
-                            df[date] = df[date].apply(self.roc_to_ad)
-                            break
-                    print(updated_api_url, df)
-                else:
-                    df2 = self.fetch_json_to_df(updated_api_url)
-                    print(updated_api_url, df2) if len(df2) != 0 else None
-                    df = pd.concat([df, df2], ignore_index=True)
-            print("  Write to csv")
-            if df is not None:
-                with open(csv_file_path, "a") as f:
-                    df.to_csv(f, index=False, header=False)
-                print(df.tail(10))
-            # Update latest_data_date
-            self.dic_latest_data_date[api_name] = latest_data_date
-        print("\n", self.dic_latest_data_date, "\n")
+            self.update_config_latest_data_date(api_name, df.iloc[-1])
+
+
+#        else:
+#            print("Update csv file...")
+#            df = None
+#            while latest_data_date < today:
+#                latest_data_date += timedelta(days=1)
+#                # Append parameters to the original URL
+#                if api_name == "API1":
+#                    date_str = self.date_to_roc_year_str(latest_data_date)
+#                else:
+#                    date_str = latest_data_date.strftime("%Y/%m/%d")
+#                print("  Current date:", date_str)
+#                updated_api_url = api_url + date_str
+#                if df is None:
+#                    df = self.fetch_json_to_df(updated_api_url)
+#                    for date in ["日期", "date", "transDate"]:
+#                        if df is not None and date in df.columns:
+#                            df[date] = df[date].apply(self.roc_to_ad)
+#                            break
+#                    print(updated_api_url, df)
+#                else:
+#                    df2 = self.fetch_json_to_df(updated_api_url)
+#                    print(updated_api_url, df2) if len(df2) != 0 else None
+#                    df = pd.concat([df, df2], ignore_index=True)
+#            print("  Write to csv")
+#            if df is not None:
+#                with open(csv_file_path, "a") as f:
+#                    df.to_csv(f, index=False, header=False)
+#                print(df.tail(10))
+#            # Update latest_data_date
+#            self.dic_latest_data_date[api_name] = latest_data_date
 
 
 if __name__ == "__main__":
     etl_processor = ExchangeFetcher("api_config_meat.json")
-    # for i in range(2):
-    #    api_live_list_day = [f"API{i}" for i in range(8, 10)]
-    #    for api in api_live_list_day:
-    #        etl_processor.process_api(api)
+    for i in range(1):
+        api_live_list_day = [f"API{i}" for i in range(8, 9)]
+        for api in api_live_list_day:
+            etl_processor.process_api(api)
