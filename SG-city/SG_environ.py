79a9d266b8141a8883819b7db79fa5944a793a26
yctsaiyc <yctsaiyc@gmail.com>
2024-05-02 15:00:28 +0800

    SG-env: Complete temperature, humidity

--- SG-city/SG_environ.py
+++ SG-city/SG_environ.py
@@ -1,81 +1,52 @@
 import os
+import requests
 import json
+import pandas as pd
+from datetime import datetime
 
 
-def DATA_GOV_SG_api(param_dict):
-    
-    register = os.path.join(self.param_dict['destination'], 'register')
-    # Make the GET request
-    response = requests.get(self.param_dict['url'])
+def DATA_GOV_SG_api(config):
+
+    response = requests.get(config["url"])
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
+        print(config["name"])
         data_json = response.json()
-
-        for doc_needed, doc_json_path in zip(self.param_dict['docs_needed'], self.param_dict['docs_json_path']):
-
-            json_paths = doc_json_path.split(',')
-            
-            # DataFrame is a two-dimensional, size-mutable, and tabular data structure.
-            dataframe = pd.DataFrame()
-            for json_path in json_paths:
-
-                data_subset = eval(f"data_json{json_path}")
-
-                # dict
-                if isinstance(data_subset, dict):
-                    new_dataframe = pd.json_normalize(data_subset)
-                    dataframe = pd.concat([dataframe, new_dataframe], ignore_index=True)
-                
-                # list
-                elif isinstance(data_subset, list):
-
-                    list_has_dict = False
-                    for item in data_subset:
-                        if isinstance(item, dict):
-                            list_has_dict = True
-                            break
-                    
-                    # dict
-                    if list_has_dict == True:
-                        new_dataframe = pd.json_normalize(data_subset)
-                        dataframe = pd.concat([dataframe, new_dataframe], ignore_index=True)
-                    
-                    # list
-                    else:
-                        matches = re.findall(r'\[(.*?)\]', json_path)
-                        column_name = eval(matches[-1])
-                        for item in data_subset:
-                            new_dataframe = pd.DataFrame({column_name: [item]})
-                            dataframe = pd.concat([dataframe, new_dataframe], ignore_index=True)
-                
-                # value
-                else:
-                    matches = re.findall(r'\[(.*?)\]', json_path)
-                    column_name = eval(matches[-1])
-                    dataframe[column_name] = data_subset
-                    # new_dataframe = pd.DataFrame({column_name: [data_subset]})
-                    # dataframe = pd.concat([dataframe, new_dataframe], ignore_index=True)
-
-
-
-            csv = os.path.join(register, doc_needed)
-
-            tz_taipei = pytz.timezone('Asia/Taipei')
-            datetime_taipei = datetime.now(tz_taipei)
-            dataframe['data_time'] = datetime_taipei.strftime("%Y-%m-%d %H:%M:%S")
-
-            dataframe.to_csv(csv, index=False)
+        df = pd.DataFrame()
+        if config["nested_data"] != "":
+            df = pd.json_normalize(eval(f"data_json{config['nested_data']['path_1']}"))
+            df = df[config["nested_data"]["fields_1"]]
+            df2 = pd.json_normalize(eval(f"data_json{config['nested_data']['path_2']}"))
+            df2 = df2[config["nested_data"]["fields_2"]]
+            df = pd.merge(
+                df,
+                df2,
+                how="inner",
+                left_on=config["nested_data"]["merge_key_1"],
+                right_on=config["nested_data"]["merge_key_2"],
+            )
+            df.drop(columns=[config["nested_data"]["merge_key_2"]], inplace=True)
+        for col in config["data_path"]:
+            df[col.split("'")[-2]] = eval(f"data_json{col}")
+        print(df)
+        csv_path = os.path.join(
+            config["csv_path"],
+            os.path.basename(config["csv_path"])
+            + "_"
+            + datetime.now().strftime("%y%m%d%H%M%S")
+            + ".csv",
+        )
+        df.to_csv(csv_path, index=False)
 
     else:
-        # Handle the error, e.g., print an error message
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
+        # Handle the error, e.g., print an rror message
+        print(f"Failed to retrieve data. Sttus code: {response.status_code}")
 
 
 if __name__ == "__main__":
     for d in os.listdir():
         if os.path.isdir(d) and d.startswith("Environ_"):
-            with open(f"{d}/{d}.json", 'r') as file:
-                param_dict = json.load(file)
-                DATA_GOV_SG_api(param_dict)
+            with open(f"{d}/{d}.json", "r") as file:
+                config = json.load(file)
+                DATA_GOV_SG_api(config)
