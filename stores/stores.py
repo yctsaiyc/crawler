8d8b714156caa725e17123bf856d86bff6a6d6a4
yctsaiyc <yctsaiyc@gmail.com>
2024-05-16 17:11:00 +0800

    Stores: Complete tgos

--- stores/stores.py
+++ stores/stores.py
@@ -3,57 +3,71 @@ from tgos import get_addr_info
 import os
 import requests
 import pandas as pd
-import time
+from tqdm import tqdm
 
 
-store_list = ["cosmed"]
+STORE_LIST = ["cosmed"]
 
 def get_cosmed_df():
     # Send a GET request to the API
     url = "https://www.cosmed.com.tw/api/getStore.aspx?t=store&c=&d=&s="
     response = requests.get(url)
-    # time.sleep(5)
     
     # Parse the JSON response
     data = response.json()
     
     # Initialize lists to store data
     store_names = []
-    zip_code_names_1 = []
-    zip_code_names_2 = []
-    addresses = []
+    addrs = []
     
     # Extract data from each store entry
     for store in data["data"]:
         store_names.append(store["StoreNM"])
-        zip_code_names_1.append(store["ZipCodeName1"])
-        zip_code_names_2.append(store["ZipCodeName2"])
-        addresses.append(store["Address"])
-    
-    # Concatenate zip code names and address for column 2
-    addresses = [f"{z1}{z2}{addr.split('(')[0]}" for z1, z2, addr in zip(zip_code_names_1, zip_code_names_2, addresses)]
+        addrs.append(store["Address"])
     
     df = pd.DataFrame({
         "Store Name": store_names,
-        "City": zip_code_names_1,
-        "District": zip_code_names_2,
-        "Address": addresses
+        "Raw Address": addrs
     })
 
     return df
 
+def clean_addr(addr):
+    addr = addr.split("號")[0]
+    addr = addr.split(" ")[0]
+    addr = addr.split("、")[0]
+    addr = addr.split(".")[0] 
+    addr += "號"
+    return addr
 
 if __name__ == "__main__":
     df = pd.DataFrame()
-    for store in store_list:
+
+    for store in STORE_LIST:
         if not os.path.exists(f"{store}.csv"):
             df = get_cosmed_df()
             df.to_csv(f"{store}.csv", index=False)
         else:
             df = pd.read_csv(f"{store}.csv")
 
+        total_iterations = len(df)
+        progress_bar = tqdm(total=total_iterations, desc="Processing")
+
         for idx, row in df.iterrows():
-            addr = row["Address"].split("號")[0].split("、")[0] + "號"
-            df.at[idx, "latitude"], df.at[idx, "longitude"], df.at[idx, "code_base"], df.at[idx, "code1"], df.at[idx, "code2"] = get_addr_info(addr)
+            addr = clean_addr(row["Raw Address"])
+            tgos_dict = get_addr_info(addr)
+
+            df.at[idx, "Address"] = tgos_dict["address"]
+            df.at[idx, "County"] = tgos_dict["county"]
+            df.at[idx, "Town"] = tgos_dict["town"]
+            df.at[idx, "Longitude"] = tgos_dict["longitude"]
+            df.at[idx, "Latitude"] = tgos_dict["latitude"]
+            df.at[idx, "Code Base"] = tgos_dict["code_base"]
+            df.at[idx, "Code1"] = tgos_dict["code1"]
+            df.at[idx, "Code2"] = tgos_dict["code2"]
+
+            progress_bar.update(1)
+
+        progress_bar.close()
 
     df.to_csv("cosmed_tgos.csv", index=False)
--- stores/tgos.py
+++ stores/tgos.py
@@ -1,7 +1,42 @@
+import requests
+import json
+
+
+KEY = input("Enter TGOS keystr: ")
+
+
 def get_addr_info(address):
-    latitude = "TODO"
-    longitude = "TODO"
-    code_base = "TODO"
-    code1 = "TODO"
-    code2 = "TODO"
-    return latitude, longitude, code_base, code1, code2
+    url = f"https://gis.tgos.tw/TGLocator/TGLocator.ashx?format=json&input={address}&keystr={KEY}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    data_dict = {
+        "address": "",
+        "county": "",
+        "town": "",
+        "longitude": "",
+        "latitude": "",
+        "code_base": "",
+        "code1": "",
+        "code2": "",
+    }
+
+    try:
+        data = data["results"][0]
+
+        data_dict["address"] = data["FULL_ADDR"]
+        data_dict["county"] = data["COUNTY"]
+        data_dict["town"] = data["TOWN"]
+        data_dict["longitude"] = data["geometry"]["x"]
+        data_dict["latitude"] = data["geometry"]["y"]
+        data_dict["code_base"] = data["CODEBASE"]
+        data_dict["code1"] = data["CODE1"]
+        data_dict["code2"] = data["CODE2"]
+
+    except IndexError:
+        pass
+
+    except KeyError:
+        print(data["error_message"])
+
+    return data_dict
